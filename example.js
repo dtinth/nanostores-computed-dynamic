0698bcda18567278fa7abe5881c53b039034b80b
dtinth on MBP M1 <dtinth@spacet.me>
2023-06-28 21:55:44 +0700

    update examples

--- example.js
+++ example.js
@@ -1,36 +1,75 @@
-import { atom } from 'nanostores'
+import { atom, computed, map } from 'nanostores'
 import { computedDynamic } from 'nanostores-computed-dynamic'
 
-const $shape = atom('circle')
-const $radius = atom(4)
-const $width = atom(2)
-const $height = atom(3)
+{
+  console.log('# Example 1')
 
-const $area = computedDynamic((use) => {
-  // `use($store)` is like `$store.get()` but it also tracks the dependency.
-  // If you use `$store.get()` here, it will not be recalculated when `$store` changes.
-  if (use($shape) === 'circle') {
-    return Math.PI * use($radius) ** 2
-  }
-  if (use($shape) === 'rectangle') {
-    return use($width) * use($height)
-  }
-})
+  const $shape = atom('circle')
+  const $radius = atom(4)
+  const $width = atom(2)
+  const $height = atom(3)
 
-$area.subscribe((value) => console.log(`Area is ${value}`))
-// The first time, it will subscribe to $shape and $radius.
-// => Area is 50.26548245743669
+  const $area = computedDynamic((use) => {
+    // `use($store)` is like `$store.get()` but it also tracks the dependency.
+    // If you use `$store.get()` here, it will not be recalculated when `$store` changes.
+    if (use($shape) === 'circle') {
+      return Math.PI * use($radius) ** 2
+    }
+    if (use($shape) === 'rectangle') {
+      return use($width) * use($height)
+    }
+  })
 
-$shape.set('rectangle')
-// $radius is not used anymore so it is unsubscribed.
-// $width and $height are subscribed instead.
-// => Area is 6
+  $area.subscribe((value) => console.log(`Area is ${value}`))
+  // The first time, it will subscribe to $shape and $radius.
+  // => Area is 50.26548245743669
 
-$radius.set(10)
-// Since $radius is no longer subscribed, it will not trigger a recalculation.
+  $shape.set('rectangle')
+  // $radius is not used anymore so it is unsubscribed.
+  // $width and $height are subscribed instead.
+  // => Area is 6
 
-$width.set(5)
-// => Area is 15
+  $radius.set(10)
+  // Since $radius is no longer subscribed, it will not trigger a recalculation.
 
-$height.set(6)
-// => Area is 30
+  $width.set(5)
+  // => Area is 15
+
+  $height.set(6)
+  // => Area is 30
+}
+
+console.log()
+
+{
+  console.log('# Example 2')
+
+  const $users = map({
+    u1: { name: 'John', feeling: '😀' },
+    u2: { name: 'Mary', feeling: '😀' },
+  })
+
+  const $onlineUserIds = atom(['u1'])
+
+  const $userById = (id) => computed($users, (users) => users[id])
+
+  const $onlineUsers = computedDynamic((use) => {
+    const onlineUserIds = use($onlineUserIds)
+    return onlineUserIds.map((id) => use($userById(id)))
+  })
+  $onlineUsers.subscribe((users) =>
+    console.log(
+      `Online users: ${users.map((u) => `${u.name} ${u.feeling}`).join(', ')}`,
+    ),
+  )
+  // => Online users: John 😀
+
+  $onlineUserIds.set(['u2'])
+  // => Online users: Mary 😀
+
+  $users.setKey('u1', { name: 'John', feeling: '😐' })
+  // Since `u1` is no longer subscribed to, it will not trigger a recalculation.
+
+  $users.setKey('u2', { name: 'Mary', feeling: '😐' })
+  // => Online users: Mary 😐
+}
